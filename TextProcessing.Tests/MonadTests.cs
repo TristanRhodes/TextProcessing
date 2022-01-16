49da137004273920059e4119f4a7890632aef399
Tristan Rhodes <tristan@appyparking.com>
2022-01-16 18:33:18 +0000

    add some comments and functor demo to the Monad test suite.

--- TextProcessing.Tests/MonadTests.cs
+++ TextProcessing.Tests/MonadTests.cs
@@ -31,14 +31,18 @@ namespace TextProcessing.Tests
         {
             Func<int> baseValue = () => 0;
 
+            // A Function that takes a function and returns a function that adds 1 to 
+            // the result.
             Func<Func<int>, Func<int>> add = (Func<int> baseValue) =>
                 () => baseValue() + 1;
 
-            Func<int> result = 
+            // A function returning a baseValue() inside an Add(), inside an Add()
+            Func<int> result =
                 add(
                     add(
                         baseValue));
 
+            // Because we ran add() twice, we get a value of 2.
             result()
                 .Should().Be(2);
         }
@@ -48,17 +52,21 @@ namespace TextProcessing.Tests
         {
             Func<int> baseValue = () => 0;
 
+            // A Function that takes value and a function and returns a function that 
+            // adds the value to the result of the function.
             Func<int, Func<int>, Func<int>> add =
                 (int value, Func<int> baseValue) =>
                     () => baseValue() + value;
 
-            Func<int> total =
+            // A function returning a baseValue, inside an add(1), inside an add(2), 
+            // inside an add(3)
+            Func<int> result =
                 add(3,
                     add(2,
                         add(1,
                             baseValue)));
 
-            total()
+            result()
                 .Should().Be(6);
         }
 
@@ -84,16 +92,48 @@ namespace TextProcessing.Tests
         }
 
         [Fact]
+        public void MonadNestingFunctorTest()
+        {
+            Func<int> baseValue = () => 0;
+
+            // A Function that takes value and a function and returns a function that 
+            // adds the value to the result of the function.
+            Func<int, Func<int>, Func<int>> add =
+                (int value, Func<int> baseValue) =>
+                    () => baseValue() + value;
+
+            // Function that takes a Func<int> and returns a Func<string> that runs the Func<int>,
+            // turns the result into a string and prefixes it with the £ symbol
+            Func<Func<int>, Func<string>> asGBPString =
+                (Func<int> baseValue) =>
+                    () => $"£" + baseValue();
+
+            // A function returning a baseValue, inside an add(1), inside an add(2), 
+            // inside an add(3), inside an asString()
+            Func<string> price =
+                asGBPString(
+                    add(3,
+                        add(2,
+                            add(1,
+                                baseValue))));
+
+            price()
+                .Should().Be("£6");
+        }
+
+        [Fact]
         public void MonadChainFluentTest()
         {
-            Func<int> total =
+            Func<string> price =
                 Beginning
                     .With(0)
                     .Add(1)
                     .Add(2)
-                    .Add(3);
+                    .Add(3)
+                    .AsGBPString();
 
-            total().Should().Be(6);
+            price()
+                .Should().Be("£6");
         }
     }
 
@@ -107,5 +147,8 @@ namespace TextProcessing.Tests
     {
         public static Func<int> Add(this Func<int> baseValue, int value) =>
             () => baseValue() + value;
+
+        public static Func<string> AsGBPString(this Func<int> baseValue) =>
+            () => $"£" + baseValue();
     }
 }
